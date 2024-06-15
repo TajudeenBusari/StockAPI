3d379290f441a6b0f9a55236236cf163f2990f54
tajud <tajud@TAJUDEEN>
2024-06-15 20:21:58 +0300

    integration test concluded for stock api

--- Stock.API.IntegrationTests2/obj/Debug/net8.0/Stock.API.IntegrationTests2.AssemblyInfo.cs
+++ Stock.API.IntegrationTests2/obj/Debug/net8.0/Stock.API.IntegrationTests2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stock.API.IntegrationTests2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccd3e9bbed89fa5749ed0a88b55d5c6be4fe1359")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef76f7041fb9a6d1253698bc8639d9fb03795a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stock.API.IntegrationTests2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stock.API.IntegrationTests2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
